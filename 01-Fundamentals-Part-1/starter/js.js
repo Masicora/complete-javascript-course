1f5fa7198db5508af557ef56ec0290de01c1a587
Jonathan Bidwell <jbidwellb@gmail.com>
2022-06-12 14:57:49 -0400

    a

---
+++ 01-Fundamentals-Part-1/starter/js.js
@@ -0,0 +1,7 @@
+let js = 'awesome';
+if (js == 'awesome') alert('Javascript is awesome');
+console.log(5 - 4 + 5 * 6);
+
+let firstName = "Gatito";
+console.log(firstName);
+console.log("ñsldkfjadsñlfkasjd");
